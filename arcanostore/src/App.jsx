8540d2995ce5a65d849deea0662228eec945f824
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2025-10-02 15:43:29 -0500

    inicio
    retirei e acionei uma estrutura simples

---
+++ arcanostore/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './componentes_principais/Navbar';
+
+function App() {
+  return (
+    <div className="body">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
