56d6e64b063a004c9e161674675285f301724285
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2025-10-06 16:44:56 -0500

    modificao do db registro
    to pensando um pouco de como fazer isso. meio dificil uma solução razoavel para isso funcionar

--- arcanostore/src/componente_sistema/db_registro.php
+++ arcanostore/src/componente_sistema/db_registro.php
@@ -1,7 +1,4 @@
 <?php
-try{
-    
-}catch
-
-
+    // nao sei oque fazer aqui mesmo. tava pensando em colocar uma criptografia como o password_hash() que seria 100% bom para a proteção de dados.
+    // Não sei se eu conseguiria fazer algum nesse nivel
 ?>
