4e8b039f4334d1d502c68e39eb01980987e4cba2
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2025-10-09 15:02:37 -0500

    verififcação de pagina
    voltei para a pagina original ou home

--- arcanostore/src/App.jsx
+++ arcanostore/src/App.jsx
@@ -1,13 +1,13 @@
 import Header from './componentes_principais/Navbar';
 import Footer from './componentes_principais/Footer';
-// import Main from './componetes_secundarios/Main_home';
-import Mains from './componetes_secundarios/Main_sobre';
+import Main from './componetes_secundarios/Main_home';
+// import Mains from './componetes_secundarios/Main_sobre';
 
 function App() {
   return (
     <div className="body">
       <Header/>
-      <Mains/>
+      <Main/>
       <Footer/>
     </div>
   );
