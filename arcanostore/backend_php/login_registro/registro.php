43b211189f0cfb7d22caad8dd95fbde0bd4aaedf
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2025-11-26 11:52:56 -0500

    pack
    so pack

--- arcanostore/backend_php/login_registro/registro.php
+++ arcanostore/backend_php/login_registro/registro.php
@@ -1,10 +1,7 @@
 <?php
-    // CONFIGURAÇÃO DE DEBUG E CORS
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-
-    // Cabeçalhos CORS e de Método
     header("Access-Control-Allow-Origin: http://localhost:3000"); 
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type"); 
